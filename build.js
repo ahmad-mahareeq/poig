6b26662acdfdc465fff6eeccdb9e30ee07a39a22
Ahmad Mahareeq <ahmadmahareeq@Ahmads-MacBook-Air.local>
2026-06-29 17:50:14 +0300

    fix build.js: use CJS instead of ESM

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
-import { readFileSync, writeFileSync } from 'fs';
+const { readFileSync, writeFileSync } = require('fs');
 const hash = (process.env.VERCEL_GIT_COMMIT_SHA || '0000000').slice(0, 7);
-const files = ['index.html'];
-files.forEach(f => {
+['index.html'].forEach(f => {
   let c = readFileSync(f, 'utf8');
   c = c.replace(/\?v=__HASH__/g, '?v=' + hash);
   writeFileSync(f, c);
